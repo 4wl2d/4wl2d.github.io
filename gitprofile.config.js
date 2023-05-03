98d3cb55d21df2e5eca5baab2ce480a1d6801186
4wl2d <94801925+4wl2d@users.noreply.github.com>
2023-05-03 12:28:00 +0300

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -6,8 +6,8 @@ const config = {
     sortBy: 'stars', // stars | updated
     limit: 4, // How many projects to display.
     exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      forks: true, // Forked projects will not be displayed if set to true.
+      projects: ['4wl2d'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
@@ -75,7 +75,7 @@ const config = {
     },
   ],
   themeConfig: {
-    defaultTheme: 'procyon',
+    defaultTheme: 'winter',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
